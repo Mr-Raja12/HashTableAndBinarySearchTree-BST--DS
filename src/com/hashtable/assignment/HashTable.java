229b1687cc14083570a09a71f478808fe7c19450
Mr-Raja12 <rk9576899082@gmail.com>
2022-12-12 16:45:55 +0530

    Added: To find Frequency of Words In Paragraph Pharse

--- src/com/hashtable/assignment/HashTable.java
+++ src/com/hashtable/assignment/HashTable.java
@@ -1,6 +1,10 @@
 //Problems1 --> Ability to find frequency of words in a sentence i.e “To be or not to be”.
+//Problems2 --> Ability to find frequency of words in a large paragraph phrase i.e
+//“Paranoid are not paranoid because they are paranoid but because they keep 
+//putting themselves deliberately into paranoid avoidable situations”
 
 package com.hashtable.assignment;
+
 /**
  * 
  * @author Raja
@@ -9,120 +13,108 @@ package com.hashtable.assignment;
 
 public class HashTable {
 
-    /*
-     * Nested class
-     */
-    static class myMapNode {
-        /*
-         * Define key value pair for hashTable
-         */
-        String key;
-        int value;
-        myMapNode next;
+	/*
+	 * Nested class
+	 */
+	static class myMapNode {
+		/*
+		 * Define key value pair for hashTable
+		 */
+		String key;
+		int value;
+		myMapNode next;
 
-        /*
-         * Declaring parameterized constructor
-         */
-        myMapNode(String key, int value)
-        {
-            this.key = key;
-            this.value = value;
-            this.next = null;
-        }
-    }
+		/*
+		 * Declaring parameterized constructor
+		 */
+		myMapNode(String key, int value) {
+			this.key = key;
+			this.value = value;
+			this.next = null;
+		}
+	}
 
-    myMapNode head = null;
-    myMapNode tail = null;
+	myMapNode head = null;
+	myMapNode tail = null;
 
-    /*
-     * Adding to HashTable. (by means of LinkedList)
-     */
-    public void addToHashTable(String data, int occurrence)
-    {
-        myMapNode newNode = new myMapNode(data, occurrence);
-        if (head == null)
-        {
+	/*
+	 * Adding to HashTable. (by means of LinkedList)
+	 */
+	public void addToHashTable(String data, int occurrence) {
+		myMapNode newNode = new myMapNode(data, occurrence);
+		if (head == null) {
 
-            head = newNode;
-        }
-        else
-        {
-            tail.next = newNode;
-            newNode.next = null;
-        }
-        tail = newNode;
-    }
+			head = newNode;
+		} else {
+			tail.next = newNode;
+			newNode.next = null;
+		}
+		tail = newNode;
+	}
 
-    /*
-     * Print HashTable.
-     */
-    public void printHashTable()
-    {
-        myMapNode current = head;
-        if (head == null) {
-            System.out.println("HashTable is empty");
-            return;
-        } else {
-            System.out.println("Occurrence of word in Sentence");
-            System.out.println("Key && Occurrence");
-            System.out.println("-----------------");
-            while (current != null) {
-                System.out.println(current.key + "    " + current.value);
-                current = current.next;
-            }
-        }
-    }
+	/*
+	 * Printing the HashTable.
+	 */
+	public void printHashTable() {
+		myMapNode current = head;
+		if (head == null) {
+			System.out.println("HashTable is empty");
+			return;
+		} else {
+			System.out.println("Occurrence of word in Paragraph");
+			System.out.println("Key && Occurrence");
+			System.out.println("-----------------");
+			while (current != null) {
+				System.out.println(current.key + "    " + current.value);
+				current = current.next;
+			}
+		}
+	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    	HashTable hashTable = new HashTable();
+		HashTable hashTable = new HashTable();
 
-        /*
-         * Declaring the sentence
-         */
-        String sentence = "To be or not to be";
+		/*
+		 * Declaring the sentence
+		 */
+		String paragraph = "Paranoids are not paranoid because they are paranoid \n"
+				+ "but because they keep putting themselves deliberately into paranoid avoidable situations";
 
-        /*
-         * Create HashTable
-         */
-        createHashTable(sentence,hashTable);
+		// Create HashTable
+		createHashTable(paragraph, hashTable);
 
-        /*
-         * Printing the HashTable
-         */
-        System.out.println("\nPrinting the Hash Table");
-        hashTable.printHashTable();
-    }
+		/*
+		 * Printing the HashTable
+		 */
+		System.out.println("\nPrinting the Hash Table");
+		hashTable.printHashTable();
+	}
 
-    public static void createHashTable(String sentence, HashTable hashTable)
-    {
-        /*
-         * Creating the string array
-         */
-        String[] words = sentence.toLowerCase().split(" ");
+	public static void createHashTable(String sentence, HashTable hashTable) {
+		/*
+		 * Creating the string array
+		 */
+		String[] words = sentence.toLowerCase().split(" ");
 
-        int[] temp = new int[words.length];
-        int visited = -1;
-        for (int i = 0; i < words.length; i++) {
-            int occurrence = 1;
-            for (int j = i + 1; j < words.length; j++)
-            {
-                if (words[i].equals(words[j]))
-                {
-                    occurrence++;
-                    temp[j] = visited;
-                }
-            }
-            if (temp[i] != visited)
-                temp[i] = occurrence;
-        }
-        for (int i = 0; i < temp.length; i++)
-        {
-            if (temp[i] != visited)
-            {
-                hashTable.addToHashTable(words[i], temp[i]);
-            }
-        }
-    }
+		int[] temp = new int[words.length];
+		int visited = -1;
+		for (int i = 0; i < words.length; i++) {
+			int occurrence = 1;
+			for (int j = i + 1; j < words.length; j++) {
+				if (words[i].equals(words[j])) {
+					occurrence++;
+					temp[j] = visited;
+				}
+			}
+			if (temp[i] != visited)
+				temp[i] = occurrence;
+		}
+		for (int i = 0; i < temp.length; i++) {
+			if (temp[i] != visited) {
+				hashTable.addToHashTable(words[i], temp[i]);
+			}
+		}
+	}
 
 }
